2e058a63fa4b3f7f781dbe310901587cb65f7242
jj <john-git@ofjj.net>
2020-10-19 18:58:38 +0200

    move camera around

--- cube3d.rb
+++ cube3d.rb
@@ -5,21 +5,93 @@ class Cube3DWindow < Gosu::Window
 	def initialize
 		@win_size = [640, 480]
 		super(*@win_size)
-		@rot_x = Math::PI/8
-		@rot_y = 0.0
-		@rot_z = 0.0
+		@cube_rot_x = Math::PI/8
+		@cube_rot_y = 0.0
+		@cube_rot_z = 0.0
+		@cam = Camera3D.new
 		@start_time = Gosu.milliseconds
 		self.caption = "Cube 3D"
-		@cube = Mesh3D.cube(:color => Gosu::Color::WHITE)
+		@cube = Mesh3D.cube(:color => Gosu::Color::WHITE).scale(0.5)
+		@keys = {}
 	end
 
 	def update
-		@rot_y = (Gosu.milliseconds - @start_time) / 1000.0
+		handle_keys
+		@cube_rot_y = (Gosu.milliseconds - @start_time) / 1000.0
 	end
 
 	def draw
-		@cube.rotate([@rot_x, @rot_y, @rot_z]).scale(0.5).to_screen(@win_size).lines.each { |src, dst|
-			Gosu.draw_line(src.x, src.y, @cube.attr[:color], dst.x, dst.y, @cube.attr[:color], 0)
+		cube = @cube.rotate([@cube_rot_x, @cube_rot_y, @cube_rot_z])
+		draw_mesh(cube)
+		#draw_mesh(Camera3D.new.project(cube).translate([0, 0, 3]))
+	end
+
+	def draw_mesh(mesh)
+		col = mesh.attr[:color]
+		@cam.project(mesh).to_screen(@win_size).lines.each { |src, dst|
+			next if not src or not dst
+			Gosu.draw_line(src.x, src.y, col, dst.x, dst.y, col, 0)
+		}
+	end
+
+	def button_down(id)
+		@keys[id] = [Gosu.milliseconds, mouse_x, mouse_y]
+	end
+
+	def button_up(id)
+		handle_keys
+		@keys.delete(id)
+	end
+
+	def handle_keys
+		cur_ts = cur_mx = cur_my = nil
+		@keys.dup.each { |k, (ts, mx, my)|
+			cur_ts ||= Gosu.milliseconds
+			cur_mx ||= mouse_x
+			cur_my ||= mouse_y
+			d_ts = (cur_ts - ts)/1000.0
+
+			case k
+			# cam translate
+			when Gosu::KB_A
+				@cam.translate!(-@cam.rightv * d_ts)
+			when Gosu::KB_D
+				@cam.translate!(@cam.rightv * d_ts)
+			when Gosu::KB_S
+				@cam.translate!(-@cam.lookv * d_ts)
+			when Gosu::KB_W
+				@cam.translate!(@cam.lookv * d_ts)
+			when Gosu::KB_C
+				@cam.translate!(-@cam.upv * d_ts)
+			when Gosu::KB_SPACE
+				@cam.translate!(@cam.upv * d_ts)
+
+			# cam rotate
+			when Gosu::KB_UP
+				@cam.rotate!(@cam.rightv * d_ts)
+			when Gosu::KB_DOWN
+				@cam.rotate!(-@cam.rightv * d_ts)
+			when Gosu::KB_LEFT
+				@cam.rotate!(@cam.upv * d_ts)
+			when Gosu::KB_RIGHT
+				@cam.rotate!(-@cam.upv * d_ts)
+			when Gosu::KB_PAGE_UP
+				@cam.rotate!(-@cam.lookv * d_ts)
+			when Gosu::KB_PAGE_DOWN
+				@cam.rotate!(@cam.lookv * d_ts)
+
+			# reset camera
+			when Gosu::KB_R
+				@cam = Camera3D.new
+
+			else
+				id_to_name = Gosu.constants.find { |k| Gosu.const_get(k) == id }
+				puts "unk key #{id_to_name || k}"
+
+				@keys.delete(k)
+				next
+			end
+			@keys[k] = [cur_ts, cur_mx, cur_my]
 		}
 	end
 end
--- mesh3d.rb
+++ mesh3d.rb
@@ -36,17 +36,17 @@ class Point3D
 		if vec[0].to_f != 0.0
 			cos = Math.cos(vec[0])
 			sin = Math.sin(vec[0])
-			y, z = y*cos + z*sin, z*cos - y*sin
+			y, z = y*cos - z*sin, y*sin + z*cos
 		end
 		if vec[1].to_f != 0.0
 			cos = Math.cos(vec[1])
 			sin = Math.sin(vec[1])
-			z, x = z*cos + x*sin, x*cos - z*sin
+			z, x = z*cos - x*sin, z*sin + x*cos
 		end
 		if vec[2].to_f != 0.0
 			cos = Math.cos(vec[2])
 			sin = Math.sin(vec[2])
-			x, y = x*cos + y*sin, y*cos - x*sin
+			x, y = x*cos - y*sin, x*sin + y*cos
 		end
 		Point3D.new(x, y, z)
 	end
@@ -55,12 +55,63 @@ class Point3D
 		rotate(vec.to_a.map { |deg| self.class.from_degree(deg) })
 	end
 
+	def clip_z(max_z)
+		self if @z <= max_z
+	end
+
+	# project to a plane sitting at screen_z, aiming for a camera at [0, 0, 0] looking at [0, 0, -1] (up [0, 1, 0])
+	# should clip_z beforehand
+	# may return nil (if z == camera_z)
+	def project_z(screen_z)
+		return if @z == 0.0
+		r = screen_z/@z
+		Point3D.new(r*@x, r*@y, screen_z)
+	end
+
+	# convert dot coordinates to screen coords (revert y axis, scale coords from 0 to size) after projection
+	def to_screen(size)
+		scale([size[1]/2.0]*3).mirror([false, true, false]).translate([size[0]/2.0, size[1]/2.0, 0.0])
+	end
+
 	def self.from_degree(angle_deg)
 		angle_deg * Math::PI / 180.0
 	end
 
 	def self.to_degree(angle_rad)
 		angle_rad * 180 / Math::PI
+	end
+
+	def -@
+		Point3D.new(-@x, -@y, -@z)
+	end
+
+	def +(other)
+		other = other.to_a
+		Point3D.new(@x+other[0].to_f, @y+other[1].to_f, @z+other[2].to_f)
+	end
+
+	def -(other)
+		other = other.to_a
+		Point3D.new(@x-other[0].to_f, @y-other[1].to_f, @z-other[2].to_f)
+	end
+
+	def *(fact)
+		scale([fact, fact, fact])
+	end
+
+	def dot(other)
+		other = other.to_a
+		@x*other[0].to_f + @y*other[1].to_f + @z*other[2].to_f
+	end
+
+	def cross(other)
+		other = other.to_a
+		ox, oy, oz = other[0].to_f, other[1].to_f, other[2].to_f
+		Point3D.new(@y*oz-@z*oy, @z*ox-@x*oz, @x*oy-@y*ox)
+	end
+
+	def to_s
+		"<Pt3D #{'%.03f' % @x} #{'%.03f' % @y} #{'%.03f' % @z}>"
 	end
 end
 
@@ -76,9 +127,9 @@ class Mesh3D
 		@lines_idx = lines_idx
 	end
 
-	# copy the mesh with updated dots, same attr/lines
-	def dup(dots=@dots)
-		Mesh3D.new(@attr, dots, @lines_idx)
+	# copy the mesh with updated dots/lines, same attr
+	def dup(dots=@dots, lines_idx=@lines_idx)
+		Mesh3D.new(@attr, dots, lines_idx)
 	end
 
 	# return an array of couples of Point3D
@@ -87,11 +138,11 @@ class Mesh3D
 	end
 
 	def translate(vec)
-		dup(@dots.map { |d| d.translate(vec) })
+		dup(@dots.map { |d| d.translate(vec) if d })
 	end
 
 	def scale_xyz(vec)
-		dup(@dots.map { |d| d.scale(vec) })
+		dup(@dots.map { |d| d.scale(vec) if d })
 	end
 
 	def scale(fact)
@@ -99,39 +150,42 @@ class Mesh3D
 	end
 
 	def rotate(vec)
-		dup(@dots.map { |d| d.rotate(vec) })
+		dup(@dots.map { |d| d.rotate(vec) if d })
+	end
+
+	def mirror(vec)
+		dup(@dots.map { |d| d.mirror(vec) if d })
 	end
 
 	def mirror_x
-		dup(@dots.map { |d| d.mirror([true, false, false]) })
+		mirror([true, false, false])
 	end
 
 	def mirror_y
-		dup(@dots.map { |d| d.mirror([false, true, false]) })
+		mirror([false, true, false])
 	end
 
 	def mirror_z
-		dup(@dots.map { |d| d.mirror([false, false, true]) })
+		mirror([false, false, true])
 	end
 
-	# project the dots coordinates to a 2d plane at z=1, camera points to [0, 0, 0]
-	# projected image covers height from y=-1 to y=+1
-	# returns a new Mesh3D with all points having z=screen_z
-	def project(camera_z=3.0, screen_z=1.0)
-		dup(@dots.map { |d|
-			if d.z >= camera_z
-				# TODO clipping ? need to work on lines
-				Point3D.new(0, 0, camera_z)
-			else
-				r = (camera_z-screen_z)/(camera_z-d.z)
-				Point3D.new(r*d.x, r*d.y, camera_z)
-			end
-		})
+	# clip dots with z > max_z
+	# split lines crossing max_z
+	def clip_z(max_z=-0.1)
+		# TODO split lines
+		dup(@dots.map { |d| d.clip_z(max_z) if d })
 	end
 
-	# project and convert dot coordinates to screen coords (revert y axis, scale coords from 0 to size)
-	def to_screen(size, *project_args)
-		project(*project_args).scale(size[1]/2.0).mirror_y.translate([size[0]/2.0, size[1]/2.0, 0.0])
+	# project the dots coordinates to a 2d plane at z=screen_z, camera sitting at [0, 0, 0] pointing to [0, 0, -1]
+	# returns a new Mesh with all points having z=screen_z
+	# should have called clip_z beforehand
+	def project_z(screen_z=-2.0)
+		dup(@dots.map { |d| d.project_z(screen_z) if d })
+	end
+
+	# convert dot coordinates to screen coords (revert y axis, scale coords from 0 to size) after projection
+	def to_screen(size)
+		scale(size[1]/2.0).mirror_y.translate([size[0]/2.0, size[1]/2.0, 0.0])
 	end
 
 	def self.cube(attr={})
@@ -143,5 +197,76 @@ class Mesh3D
 			 [0, 4], [1, 5], [2, 6], [3, 7]] # front to back
 
 		new(attr, dots.map { |d| Point3D.new(*d) }, lines)
+	end
+
+	def self.line(p_end, attr={})
+		new(attr, [Point3D.new(0, 0, 0), p_end], [[0, 1]])
+	end
+end
+
+class Camera3D
+	attr_accessor :pos, :lookv, :upv, :screen_dist, :clip_dist
+	def initialize(pos=Point3D.new(0, 0, 3), lookv=Point3D.new(0, 0, -1), upv=Point3D.new(0, 1, 0), screen_dist=2.0, clip_dist=1.0)
+		# position
+		@pos = pos
+		# look (front) vector
+		@lookv = lookv
+		# up vector
+		@upv = upv
+		# screen distance (for projection), at @pos + screen_dist*@lookv
+		@screen_dist = screen_dist
+		# clip distance (similar to screen_dist, clip stuff behind this)
+		@clip_dist = clip_dist
+	end
+
+	def rightv
+		@lookv.cross(@upv)
+	end
+
+	def rotate!(vec)
+		@lookv = @lookv.rotate(vec)
+		@upv = @upv.rotate(vec)
+	end
+
+	def translate!(vec)
+		@pos = @pos.translate(vec)
+	end
+
+	def angle_to_norm
+		# return the vector to rotate by, so that lookv and up have their default value
+		l = @lookv
+		# make y == 0 && z <= 0
+		if l.z > 0.0
+			x = Math::PI + Math.atan(l.y/l.z)
+		elsif l.z == 0.0
+			x = l.y > 0 ? -Math::PI/2 : Math::PI/2
+		else
+			x = Math.atan(l.y/l.z)
+		end
+		l = l.rotate([x, 0.0, 0.0])
+
+		# make x == 0
+		if l.z == 0.0
+			y = l.x > 0 ? Math::PI/2 : -Math::PI/2
+		else
+			y = -Math.atan(l.x/l.z)
+		end
+
+		u = @upv.rotate([x, y, 0.0])
+		# make up == y
+		if u.y < 0.0
+			z = Math::PI + Math.atan(u.x/u.y)
+		elsif u.y == 0.0
+			z = l.x > 0 ? Math::PI/2 : -Math::PI/2
+		else
+			z = Math.atan(u.x/u.y)
+		end
+
+		Point3D.new(x, y, z)
+	end
+
+	# project the <what> on the camera's screen
+	def project(what)
+		what.translate(-@pos).rotate(angle_to_norm).clip_z(-@clip_dist).project_z(-@screen_dist)
 	end
 end
